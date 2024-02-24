d5263450202afe679473c76a59668da08b81021c
AlanAkandzhi <157907242+AlanAkandzhi@users.noreply.github.com>
2024-02-24 16:15:57 +0100

    Update Program_Template.cs

--- Program_Template.cs
+++ Program_Template.cs
@@ -4,28 +4,28 @@ using AnsiTools;
 using Colors = AnsiTools.ANSICodes.Colors;
 
 Console.Clear();
-Console.WriteLine("Starting Assignment 2");
+Console.WriteLine("Starting Exam_unit_2");
 
-// SETUP 
-const string myPersonalID = "83593b863072c7c1ccb7e7f712d285758643b2c401501531c1777013eba6d04d"; // GET YOUR PERSONAL ID FROM THE ASSIGNMENT PAGE https://mm-203-module-2-server.onrender.com/
+// SETUP
+
+const string myPersonalID = "83593b863072c7c1ccb7e7f712d285758643b2c401501531c1777013eba6d04d"; 
 const string baseURL = "https://mm-203-module-2-server.onrender.com/";
-const string startEndpoint = "start/"; // baseURl + startEndpoint + myPersonalID
-const string taskEndpoint = "task/";   // baseURl + taskEndpoint + myPersonalID + "/" + taskID
+const string startEndpoint = "start/"; 
+const string taskEndpoint = "task/";   
 
-// Creating a variable for the HttpUtils so that we dont have to type HttpUtils.instance every time we want to use it
 HttpUtils httpUtils = HttpUtils.instance;
 
 //#### REGISTRATION
-// We start by registering and getting the first task
+
 Response startRespons = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
-Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n"); // Print the response from the server to the console
-string taskID = "aAaa23"; // We get the taskID from the previous response and use it to get the task (look at the console output to find the taskID)
+Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n"); 
+string taskID = "aAaa23"; 
 
 //#### FIRST TASK 
 
 Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID);
 Task task1 = JsonSerializer.Deserialize<Task>(task1Response.content);
-Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{task1?.title}{ANSICodes.Reset}\n{task1?.description}\nParameters: {Colors.Yellow}{task1?.parameters}{ANSICodes.Reset}");
+Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{task1?.title}{ANSICodes.Reset}\n{task1?.description}\nParameters: {Colors.Red}{task1?.parameters}{ANSICodes.Reset}");
 
 string tempInFahrenheitAsString = task1?.parameters;
 Console.WriteLine("This is just checking " + tempInFahrenheitAsString);
@@ -68,7 +68,7 @@ taskID = "kuTw53L";
 
 Response task3Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); 
 Task task3 = JsonSerializer.Deserialize<Task>(task3Response.content);
-Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{task3?.title}{ANSICodes.Reset}\n{task3?.description}\nParameters: {Colors.Yellow}{task3?.parameters}{ANSICodes.Reset}");
+Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{task3?.title}{ANSICodes.Reset}\n{task3?.description}\nParameters: {Colors.Red}{task3?.parameters}{ANSICodes.Reset}");
 
 string numberSequence = task3?.parameters;
 
@@ -99,12 +99,11 @@ Console.WriteLine($"Answer: {Colors.Green}{response3}{ANSICodes.Reset}");
 
 taskID = "aLp96";
 
-
 //#### FOURTH TASK
 
 Response task4Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); 
 Task task4 = JsonSerializer.Deserialize<Task>(task4Response.content);
-Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{task4?.title}{ANSICodes.Reset}\n{task4?.description}\nParameters: {Colors.Yellow}{task4?.parameters}{ANSICodes.Reset}");
+Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{task4?.title}{ANSICodes.Reset}\n{task4?.description}\nParameters: {Colors.Red}{task4?.parameters}{ANSICodes.Reset}");
 
 string enigmaticNumber = task4?.parameters;
 
@@ -123,6 +122,6 @@ class Task
     public string? title { get; set; }
     public string? description { get; set; }
     public string? taskID { get; set; }
-    public string? usierID { get; set; }
+    public string? userID { get; set; }
     public string? parameters { get; set; }
 }
